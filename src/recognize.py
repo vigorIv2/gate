98f3591b4e2cc24e98a756c29f89cc16a46802ee
Igor Vasilchikov <vigor.iv.2@gmail.com>
2016-11-07 21:49:26 -0800

    beginning to use pycharm

--- src/recognize.py
+++ src/recognize.py
@@ -18,7 +18,7 @@ logging.basicConfig(filename='/var/log/motion/recognize.log',format='%(asctime)s
 
 def add_category(fpath,category):
 	fpv = fpath.split("/")
- 	result="/".join(fpv[:-1])+"/"+category+"/"+fpv[-1]
+	result="/".join(fpv[:-1])+"/"+category+"/"+fpv[-1]
 	return result
 
 def push_button():
@@ -31,24 +31,24 @@ def push_button():
 	time.sleep(4)
 	GPIO.output(pin,True)
 	logging.info("push_button end")
- 	 
+
 def categorize_plate(category_json,image) :
 	if ( not os.path.isfile(image) ):
 		logging.warn("file not found "+image)
-		return 
+		return
 	sz = os.path.getsize(image)
 	if ( sz == 0L ):
 		logging.warn("file empty, removing "+image)
-		os.remove(image)	
+		os.remove(image)
 		return
-	try: 
+	try:
 		with open(category_json, 'r') as jsonfile:
 			cat=jsonfile.read()
 		jcat=json.loads(cat)
 		jcats=jcat["categories"]
 		# do this once for one root image path, create directory structure
 		ripp="/".join(image.split("/")[:-1])
-  		rid=mc.get("create_image_dirs"+ripp)
+		rid=mc.get("create_image_dirs"+ripp)
 		if ( not rid ):
 			for cn in [ "low_confidence", "match", "mismatch", "no_plate" ]:
 				image_path=add_category(image,jcats[cn])
@@ -56,7 +56,7 @@ def categorize_plate(category_json,image) :
 				logging.info("making sure directory "+image_path+" created")
 				if (not os.path.exists(image_path)):
 					os.makedirs(image_path)
-	  		mc.set("create_image_dirs"+ripp,1)
+			mc.set("create_image_dirs"+ripp,1)
 
 		p = subprocess.Popen([alpr_path, '-c', 'us', '-j', image], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		out, err = p.communicate()
@@ -87,7 +87,7 @@ def categorize_plate(category_json,image) :
 						mc.set(plate,time.time())
 						if ( plate in jcat['allowed_plates'] ):
 							logging.info("license plate "+plate+" recognized as allowed with confidence "+sconf+" image="+image)
-							push_button()	
+							push_button()
 							os.rename(image,add_category(image,jcats["match"]))
 							with open(add_category(image+".json",jcats["match"]), "w") as json_file:
 								json_file.write(out)
@@ -100,14 +100,14 @@ def categorize_plate(category_json,image) :
 			logging.info("no license plate recognized, moving to removed "+image)
 			os.rename(image,add_category(image,jcats["no_plate"]))
 	except Exception, err:
-        	logging.exception('Error from throws():')
+			logging.exception('Error from throws():')
 #		print sys.exc_info()
 #		logging.info("exception removing image "+image)
 #		if ( os.path.isfile(image) ): 
 #			os.remove(image)	
 
 if ( len(sys.argv) != 3 ):
-	print "Uaage: recognize_plate.py <category_definition.json> <image file name>" 
+	print "Uaage: recognize_plate.py <category_definition.json> <image file name>"
 	sys.exit(0)
 
 categorize_plate(sys.argv[1],sys.argv[2])
